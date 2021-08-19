ecfeda3a35f6dd2e74e297ad7ab89bc4a42a38f2
Sam Bevans-Kerr <sam-bk@outlook.com>
2021-08-19 16:16:41 -0400

    Comment

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Contact from './components/Contact'
 import Navbar from './components/Navbar'
 import Projects from './components/Projects'
 import Skills from './components/Skills'
+//Sam Bevans-Kerr
 
 const App = () => {
   return (
