052b7f04c65620b365da8d8977ac67f77ebe7218
Sam Bevans-Kerr <sam-bk@outlook.com>
2021-08-19 16:18:38 -0400

    Content changes

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ const About = () => {
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
             Sam Bevans-Kerr
-            <br className="hidden lg:inline-block" />Software Engineer
+            <br className="hidden lg:inline-block" />Software Developer
           </h1>
           <p className="mb-8 leading-relaxed">
           As a graduate from the Digital Arts and Sciences program in the UF College of Engineering,
